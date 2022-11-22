59bf830c89e2b013976c9b1c7fe69ddaa7ce0652
TouiaBakassi <Touria_Bakassi@outlook.com>
2022-11-22 02:12:40 +0100

    Add some

--- Home/script.js
+++ Home/script.js
@@ -2,5 +2,6 @@ var iconbar= document.querySelector(".fa-bars");
 var list= document.querySelector(".list");
 
 iconbar.addEventListener("click",()=>{
+    iconbar.classList.toggle("fa-xmark");
     list.classList.toggle("active"); 
 });
